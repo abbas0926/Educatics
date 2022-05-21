3795752107a7e9635dd4f18b583587f2a3c93bf9
Amar Neche <amarneche@gmail.com>
2022-05-21 14:28:59 +0100

    just saving

---
+++ config/cpanel.php
@@ -0,0 +1,22 @@
+<?php 
+
+return [
+    
+    'username'=>'amarneche',
+
+
+    'password'=>'Majorant132!',
+
+
+    'hostName'=>'amarneche.me',
+
+
+    'port'=>'2083',
+
+
+    'domain'=>'amarneche.me',
+
+
+    'domain_dir'=>'public_html/',
+
+];
